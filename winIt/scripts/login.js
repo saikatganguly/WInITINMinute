22d6c9ae6d69579ca1d534982e61ee51fe6d2222
Arpita Sharda <arpita.sharda@lmsin.com>
2015-04-10 09:03:53 +0000

    Changes made by Arpita Sharda (arpita.sharda@lmsin.com).

--- winIt/scripts/login.js
+++ winIt/scripts/login.js
@@ -3,21 +3,31 @@ var everlive = new Everlive({
     scheme: "https",
     token: localStorage.getItem('access-token')
 });
-
+function changeUI(){
+    if (kendo.support.mobileOS.android) {
+        /*alert(kendo.support.mobileOS);*/
+        $('#logo').css('height','125px');
+        $('.option').css('height','47px');
+        $('.login-icon').css('height','33px');
+        $('.password-icon').css('height','37px');
+    }
+}
 function beforeShow(beforeShowEvt) {
     if(app.view().id == "views/login.html") {
-      //   $("#header").hide("fast");
+         $("#header").hide("fast");
          beforeShowEvt.preventDefault();
-    //     $("#drawerBtn").hide("fast");
+         $("#drawerBtn").hide("fast");
          $("#appDrawer").hide("fast");
     }
 }
 function show(){
-   // alert("show");
+    alert("show");
        $("#header").show("fast");
 }
 function hide(){
-   // alert("hide");
+       alert("hide");
+    alert(e.view.header);
+   // e.view.header.find(".one").hide();
        $("#header").hide("fast");
 }
 function login(){
@@ -26,16 +36,16 @@ function login(){
                 var username = $("#userName").val();
                 var password = $("#password").val();
          if (validator.validate()) {
-                alert(username+"  "+password);
-                everlive.Users.login(username, password, function(data) {
+                    alert(username+"  "+password);
+                    everlive.Users.login(username, password, function(data) {
                     var accessToken = data.result.access_token;
                     alert("Successfully logged the user in! Received access token: " + accessToken);
                     localStorage.setItem('access-token', accessToken);
-                   app.navigate("views/home.html","slide"); 
-                  show();
+                    app.navigate("views/home.html","slide"); 
+                  //  show();
                 }, function(err) {
                     console.log(err.message);
-                      $("#errMsg").text("Invalid Username or Password!");
+                     $("#errMsg").text("Invalid Username or Password!");
                      $("#userName").val("");
                      $("#password").val("");
             });
@@ -48,23 +58,26 @@ function logout(){
             everlive.Users.logout(function() {
             alert("Logout successful!");
             localStorage.getItem('access-token');
-                app.navigate("views/login.html","slide");
-                hide();
+            app.navigate("views/login.html","slide");    
+          //  hide();
         }, function(err) {
             alert("Failed to logout: " + err.message);
         });
 }
-
 function isLoggedIn(){
        everlive.Users.currentUser(function(data) {
             if (data.result) {
                 var username = data.result.Username;
                  app.navigate("views/home.html","slide");
                 alert(username + " is logged in!");
+                $("#userName").val("");
+                $("#password").val("");
             } else {
                 alert("Missing access token. Please login!");
+                  app.navigate("views/login.html","slide");
             }
        }, function(err) {
             alert(err.message + " Please login.");
+            app.navigate("views/login.html","slide");
        });
 }
