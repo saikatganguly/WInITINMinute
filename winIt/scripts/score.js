582098c4a2b16d6698fb2cc12171654325c8206e
Arpita Sharda <arpita.sharda@lmsin.com>
2015-04-28 13:55:07 +0000

    Changes made by Arpita Sharda (arpita.sharda@lmsin.com).

--- winIt/scripts/score.js
+++ winIt/scripts/score.js
@@ -66,3 +66,78 @@ function login() {
         });
 
     }
+function createHello(){
+    var data = everlive.data('Hello');
+    data.create({ 'string':'Hello','name' : 'Arpita'},
+    function(data){
+        alert(data.result.Id);
+      },
+      function(error){
+        alert(JSON.stringify(error));
+      });
+}
+
+//Ajax request using jQuery
+function test(){
+    var data={};
+    $.ajax({
+        url: "https://platform.telerik.com/bs-api/v1/KGCVGkuoa3Zw0Auj/Functions/getQuestionService",
+        type: "GET",
+        headers: {
+            "Authorization" : localStorage.getItem('access-token'),
+        },
+        contentType: "application/json",
+        accept: "application/json",
+        success: function(data){
+            alert(JSON.stringify(data));
+            data.questions = JSON.stringify(data);
+            alert(data.questions);
+        },
+        error: function(error){
+            alert(JSON.stringify(error));
+        }
+    })
+}
+
+function getQestions(){
+    var data={};
+    $.ajax({
+    url: 'http://api.everlive.com/v1/KGCVGkuoa3Zw0Auj/questions',
+    type: "GET",
+    headers: {
+        "Authorization" : localStorage.getItem('access-token')
+    },
+    success: function(data){
+         data.questions = data.Result;
+         alert(JSON.stringify(data.questions));
+    },
+    error: function(error){
+        alert(JSON.stringify(error));
+    }
+    })
+}
+/*$.ajax({
+        url: 'https://api.everlive.com/v1/KGCVGkuoa3Zw0Auj/questions',
+        type: "GET",
+        headers: {
+            "Authorization" : localStorage.getItem('access-token')
+        },
+        success: function(result_data){
+             data.questions = result_data.Result;
+           //  alert(JSON.stringify(data.questions));
+                 if(user.questionIndex<=4){
+                    changeQuection(user.questionIndex);
+                 }
+                 $('#ms_timer').countdowntimer({
+                      minutes :minutes,
+                      seconds :seconds,
+                      size : "lg",
+                      timeSeparator : ".",
+                      timeUp : timeisUp
+                });
+                isQuizVisited = true;
+        },
+        error: function(error){
+            alert(JSON.stringify(error));
+        }
+    })*/
